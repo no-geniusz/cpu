5bcd71ce8eb83927941835385d36447e044f0c90
Paweł Pelczar <paweusz@gmail.com>
2019-11-16 16:49:22 +0100

    Arbitrary width ALU

--- alu.py
+++ alu.py
@@ -1,34 +1,36 @@
-from registers import REG_WIDTH
 from adders import RCAdder
 from subtractors import RCSubtractor
 from mux import Multiplexer
 
 class Alu:
 
-    a = [None for k in range(REG_WIDTH)]
-    b = [None for k in range(REG_WIDTH)]
-    o = [None for k in range(2)]
-    x = [None for k in range(REG_WIDTH)]
+    def __init__(self, width):
+        self.__width = width
 
-    _adder = RCAdder(REG_WIDTH)
-    _subtractor = RCSubtractor(REG_WIDTH)
-    _mux = Multiplexer(REG_WIDTH)
+        self.a = [None for k in range(width)]
+        self.b = [None for k in range(width)]
+        self.o = [None for k in range(2)]
+        self.x = [None for k in range(width)]
+
+        self.__adder = RCAdder(width)
+        self.__subtractor = RCSubtractor(width)
+        self.__mux = Multiplexer(width)
 
     def eval(self):
-        for k in range(REG_WIDTH):
-            self._adder.a[k] = self.a[k]
-            self._adder.b[k] = self.b[k]
-            self._subtractor.x[k] = self.a[k]
-            self._subtractor.y[k] = self.b[k]
-        self._adder.eval()
-        self._subtractor.eval()
+        for k in range(self.__width):
+            self.__adder.a[k] = self.a[k]
+            self.__adder.b[k] = self.b[k]
+            self.__subtractor.x[k] = self.a[k]
+            self.__subtractor.y[k] = self.b[k]
+        self.__adder.eval()
+        self.__subtractor.eval()
 
-        for k in range(REG_WIDTH):
-            self._mux.i0[k] = self._adder.s[k]
-            self._mux.i1[k] = self._subtractor.d[k]
-        self._mux.sel2 = self.o[0]
-        self._mux.sel1 = self.o[1]
-        self._mux.eval()
+        for k in range(self.__width):
+            self.__mux.i0[k] = self.__adder.s[k]
+            self.__mux.i1[k] = self.__subtractor.d[k]
+        self.__mux.sel2 = self.o[0]
+        self.__mux.sel1 = self.o[1]
+        self.__mux.eval()
 
-        for k in range(REG_WIDTH):
-            self.x[k] = self._mux.out[k]
+        for k in range(self.__width):
+            self.x[k] = self.__mux.out[k]
---
+++ alu_test.py
@@ -0,0 +1,16 @@
+from alu import Alu
+
+def test_alu():
+    alu = Alu(4)
+
+    alu.a = [0, 0, 0, 1]
+    alu.b = [0, 1, 1, 0]
+    alu.o = [0, 0]
+    alu.eval()
+
+    assert alu.x == [0, 1, 1, 1]
+
+    alu.o = [1, 0]
+    alu.eval()
+
+    assert alu.x == [0, 1, 0, 0]
