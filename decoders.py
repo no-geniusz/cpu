078042d8b72420d8e49afc64a69123b508294e18
Pawel Pelczar <pawel.pelczar@ista.com>
2019-11-23 09:13:15 +0100

    Enable signal and 16 bit decoder

--- decoders.py
+++ decoders.py
@@ -1,4 +1,5 @@
 from gates import AndGate, NotGate
+import math
 
 class Decoder2By4:
     
@@ -11,37 +12,93 @@ class Decoder2By4:
         self.d2 = None
         self.d3 = None
 
-        self.__not0 = NotGate()
-        self.__not1 = NotGate()
+        self.__nots = [NotGate() for _ in range(2)]
+        self.__ands = [AndGate() for _ in range(4)]
 
-        self.__and0 = AndGate()
-        self.__and1 = AndGate()
-        self.__and2 = AndGate()
-        self.__and3 = AndGate()
+    def eval_internal(self):
+        self.__nots[0].a = self.a
+        self.__nots[0].eval()
+        self.__nots[1].a = self.b
+        self.__nots[1].eval()
+
+        self.__ands[0].a = self.__nots[0].q
+        self.__ands[0].b = self.__nots[1].q
+        self.__ands[0].eval()
+
+        self.__ands[1].a = self.a
+        self.__ands[1].b = self.__nots[1].q
+        self.__ands[1].eval()
+
+        self.__ands[2].a = self.b
+        self.__ands[2].b = self.__nots[0].q
+        self.__ands[2].eval()
+
+        self.__ands[3].a = self.b
+        self.__ands[3].b = self.a
+        self.__ands[3].eval()
 
     def eval(self):
-        self.__not0.a = self.a
-        self.__not0.eval()
-        self.__not1.a = self.b
-        self.__not1.eval()
+        self.eval_internal()
 
-        self.__and0.a = self.__not0.q
-        self.__and0.b = self.__not1.q
-        self.__and0.eval()
+        self.d0 = self.__ands[0].q
+        self.d1 = self.__ands[1].q
+        self.d2 = self.__ands[2].q
+        self.d3 = self.__ands[3].q
 
-        self.__and1.a = self.a
-        self.__and1.b = self.__not1.q
-        self.__and1.eval()
+class Decoder2By4E:
+    
+    def __init__(self):
+        self.enable = None
 
-        self.__and2.a = self.b
-        self.__and2.b = self.__not0.q
-        self.__and2.eval()
+        self.a = None
+        self.b = None
 
-        self.__and3.a = self.b
-        self.__and3.b = self.a
-        self.__and3.eval()
+        self.d0 = None
+        self.d1 = None
+        self.d2 = None
+        self.d3 = None
 
-        self.d0 = self.__and0.q
-        self.d1 = self.__and1.q
-        self.d2 = self.__and2.q
-        self.d3 = self.__and3.q
+        self.__decoder_2_by_4 = Decoder2By4()
+        self.__enable_ands = [AndGate() for _ in range(4)]
+
+    def eval(self):
+        self.__decoder_2_by_4.a = self.a
+        self.__decoder_2_by_4.b = self.b
+
+        for k in range(4):
+            setattr(self.__decoder_2_by_4, f'd{k}', getattr(self, f'd{k}'))
+
+        self.__decoder_2_by_4.eval()
+
+        for k in range(4):
+            self.__enable_ands[k].a = getattr(self.__decoder_2_by_4, f'd{k}')
+            self.__enable_ands[k].b = self.enable
+            self.__enable_ands[k].eval()
+
+        self.d0 = self.__enable_ands[0].q
+        self.d1 = self.__enable_ands[1].q
+        self.d2 = self.__enable_ands[2].q
+        self.d3 = self.__enable_ands[3].q
+
+class Decoder4By16:
+    
+    def __init__(self):
+        self.a = [None] * 4
+        self.d = [None] * 0x10
+
+        self.__e_decs = [Decoder2By4E() for _ in range(4)]
+        self.__entry_dec = Decoder2By4()
+
+    def eval(self):
+        self.__entry_dec.b = self.a[0]
+        self.__entry_dec.a = self.a[1]
+        self.__entry_dec.eval()
+
+        for k in range(4):
+            self.__e_decs[k].b = self.a[2]
+            self.__e_decs[k].a = self.a[3]
+            self.__e_decs[k].enable = getattr(self.__entry_dec, f'd{k}')
+            self.__e_decs[k].eval()
+        
+        for k in range(0x10):
+            self.d[k] = getattr(self.__e_decs[math.floor(k / 4)], f'd{k % 4}')
--- decoders_test.py
+++ decoders_test.py
@@ -1,4 +1,4 @@
-from decoders import Decoder2By4
+from decoders import Decoder2By4, Decoder2By4E, Decoder4By16
 
 def test_decoder00():
     decoder = Decoder2By4()
@@ -47,3 +47,27 @@ def test_decoder11():
     assert decoder.d1 == 0
     assert decoder.d2 == 0
     assert decoder.d3 == 1
+
+def test_enable():
+    decoder = Decoder2By4E()
+
+    decoder.a = 0
+    decoder.b = 0
+
+    decoder.enable = 0
+    decoder.eval()
+    assert decoder.d0 == 0
+
+    decoder.enable = 1
+    decoder.eval()
+    assert decoder.d0 == 1
+
+def test_4_by_16():
+    decoder = Decoder4By16()
+
+    for k in range(0xF):
+        decoder.a = [k & 0x8, k & 0x4, k & 0x2, k & 0x1]
+        decoder.eval()
+
+        for j in range(0, 0xF):
+            assert decoder.d[j] == (k == j)
